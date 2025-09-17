890729a9158e1523518109e79ab4a82eddbf87d7
Dennis Metzger <dennis.metzger@gmail.com>
2025-09-17 08:27:49 -0400

    Updated SyncMe script, use the variable {Drive} for destination

--- SyncMe.sh
+++ SyncMe.sh
@@ -1,11 +1,16 @@
 # CheckSum="-c"
 CheckSum=""
-
-echo "Syncing: PhotoArch"
-rsync -av ${CheckSum} --delete --exclude-from=/Users/${USER}/.rsync-ignore /Volumes/dockdata/PhotoArch /Volumes/MediaArch
-
-echo "Syncing: VideArch"
-rsync -av ${CheckSum} --delete --exclude-from=/Users/${USER}/.rsync-ignore /Volumes/dockdata/VideoArch /Volumes/MediaArch
+Drive="ext4tb01"
 
 echo "Syncing: CloudDocs"
-rsync -avL ${CheckSum} --delete --exclude-from=/Users/${USER}/.rsync-ignore "/Users/${USER}/Library/Mobile Documents/com~apple~CloudDocs" /Volumes/MediaArch
+rsync -avL ${CheckSum} --delete --exclude-from=/Users/${USER}/.rsync-ignore "/Users/${USER}/Library/Mobile Documents/com~apple~CloudDocs" /Volumes/${Drive}
+
+echo "Syncing: PhotoArch"
+rsync -av ${CheckSum} --delete --exclude-from=/Users/${USER}/.rsync-ignore /Volumes/dockdata/PhotoArch /Volumes/${Drive}
+
+echo "Syncing: Pictures"
+rsync -av ${CheckSum} --delete --exclude-from=/Users/${USER}/.rsync-ignore /Users/${USER}/Pictures/Photos Library.photoslibrary /Volumes/${Drive}
+
+echo "Syncing: VideArch"
+rsync -av ${CheckSum} --delete --exclude-from=/Users/${USER}/.rsync-ignore /Volumes/dockdata/VideoArch /Volumes/${Drive}
+
